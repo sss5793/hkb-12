42fc25d38212b393d18223385cae5d7c42ba7cab
sss5793 <tmfrl002@naver.com>
2020-07-27 23:32:17 +0900

    프론트엔드 observer 패턴 적용
    - store.js 파일을 추가하여 observer 패턴을 적용
    - observer에 공통적으로 사용할 데이터를 에시로 추가
    - 이벤트 등록하는 registerNotify 함수 생성
    - 등록된 데이터를 가져오는 getState 함수 생성
    - 데이터를 변경할 setState 함수 생성
    - index.js에서 사용하는 방법 예시로 추가
    
    코드 리뷰를 하면서 함수명이나 방법을 개선하면 좋을 것 같습니다!

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,15 @@
-const a = 12377774;
-const b = 1111112377774;
+import { registerNotify, setState,getState } from "./store";
 
-console.log(a, b);
+function init() {
+    // 초기 데이터
+    const user = getState('user');
+    console.log(user);
+    // 이벤트 등록
+    registerNotify('user',(data) => console.log(data));
+    registerNotify('account',(data) => console.log(data));
+
+    // 데이터 변경
+    setState('user', '사용자 1');
+}
+
+init();
