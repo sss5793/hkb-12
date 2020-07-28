891b3d1792bd5ceef1e475b3bba235028db6faeb
MyeongSeong <maeng2418@naver.com>
2020-07-29 00:14:28 +0900

    Eslint 프론트&백 적용 - eslint를 client와 server 둘다 적용 - 기존의 룰에 안맞는 코드 수정.

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
-import { registerEvent, setState, getState } from "./store";
+import { registerEvent, setState, getState } from './store';
 
 function init() {
-    // 초기 데이터
-    const user = getState('user');
-    console.log(user);
-    // 이벤트 등록
-    registerEvent('user',(data) => console.log(data));
-    registerEvent('account',(data) => console.log(data));
+  // 초기 데이터
+  const user = getState('user');
+  console.log(user);
+  // 이벤트 등록
+  registerEvent('user', (data) => console.log(data));
+  registerEvent('account', (data) => console.log(data));
 
-    // 데이터 변경
-    setState('user', '사용자 1');
+  // 데이터 변경
+  setState('user', '사용자 1');
 }
 
 init();
