614ba7540ddaa7b7be9f88f41d92e831dfc5f9ac
MyeongSeong <maeng2418@naver.com>
2020-07-28 09:55:27 +0900

    스토어 디렉토리 이동 및 변수, 함수명 변경 - store.js -> store/index.js - event -> eventHandler - registerNotify -> registerEvent

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,18 +1,18 @@
 const observer = {
     user : {
         data: 'user1',
-        notify : null,
+        eventHandler : null,
     },
     account : {
         data: [ '거래 1' , '거래 2'],
-        notify : null,
+        eventHandler : null,
     },
 };
 
-function registerNotify(key, event) {
+function registerEvent(key, eventHandler) {
     // 등록된 키에 이벤트를 등록해준다.
     // 데이터가 바뀌고 나서 실행하고 싶은 이벤트를 등록하면 될 것 같다.
-    observer[key].notify = event;
+    observer[key].eventHandler = eventHandler;
 }
 
 function getState (key) {
@@ -24,11 +24,11 @@ function setState (key, data) {
     // store에 등록한 키의 데이터를 업데이트 한다.
     observer[key].data = data;
     // 데이터가 변경되고 등록된 이벤트를 실행한다.
-    observer[key].notify(observer[key].data);
+    observer[key].eventHandler(observer[key].data);
 }
 
 export {
-    registerNotify,
+    registerEvent,
     setState,
     getState
 }
