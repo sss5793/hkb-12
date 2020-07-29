e14bd373f77354c30bb16eb7e91bd2d82aaf05b7
MyeongSeong <maeng2418@naver.com>
2020-07-29 13:55:10 +0900

    ESLint를 적용하고 코드를 일관되게 수정

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -1,14 +1,12 @@
 import './layout.scss';
-import Header from "./header";
-import Container from "./container";
+import Header from './header';
+import Container from './container';
 
-const Layout = (child) => {
-    return(
-        `<section class="layout">
-            ${Header()}
-            ${Container(child)}
-        </section>`
-    );
-}
+const Layout = (child) => (
+  `<section class="layout">
+      ${Header()}
+      ${Container(child)}
+    </section>`
+);
 
 export default Layout;
