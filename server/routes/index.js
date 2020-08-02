b4d16830736812ad08272860010c8bc62d49a5d8
sss5793 <tmfrl002@naver.com>
2020-08-03 00:01:51 +0900

    프론트엔드 api 및 endpoints 추가
    - apis 폴더 추가, index.js, endpoints.js 파일 추가
    - endpoints.js에 api 설계에 따른 endpoint 정리
    - apis/index.js에 fetch call 예제 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const hbkhist = require('./hkbhist');
+const hkbhist = require('./hkbhist');
 // const payment = require('./payment');
 // const category = require('./category');
 // const user = require('./user');
 
 const router = express.Router();
 
-router.use('/hbkhist', hbkhist);
+router.use('/hkbhist', hkbhist);
 // router.use('/payment', payment);
 // router.use('/category', category);
 // router.use('/user', user);
