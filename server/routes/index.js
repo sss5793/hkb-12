bbabf38fa007ab1b10c63fc670d6b57a634c92a7
김명성 <46251629+maeng2418@users.noreply.github.com>
2020-08-03 14:53:27 +0900

    Revert "프론트엔드 api와 endpoint 추가"

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const hkbhist = require('./hkbhist');
+const hbkhist = require('./hkbhist');
 // const payment = require('./payment');
 // const category = require('./category');
 // const user = require('./user');
 
 const router = express.Router();
 
-router.use('/hkbhist', hkbhist);
+router.use('/hbkhist', hbkhist);
 // router.use('/payment', payment);
 // router.use('/category', category);
 // router.use('/user', user);
